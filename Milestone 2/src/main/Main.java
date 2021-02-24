c7e9dcb9b365d00034c3a67ec9977c1eb8b262f4
Nathan Malta <n.malta@icloud.com>
2021-02-24 02:54:49 -0500

    Adds configuration window layout and implements some css formatting

--- Milestone 2/src/main/Main.java
+++ Milestone 2/src/main/Main.java
@@ -1,14 +1,20 @@
 package main;
 
 import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
+import javafx.scene.control.Button;
 import javafx.scene.layout.Pane;
 import javafx.stage.Stage;
 
+import java.util.ArrayList;
+import java.util.List;
+
 
 public class Main extends Application {
-    private int screenWidth = 800;
-    private int screenHeight = 800;
+    private int screenWidth = 1920;
+    private int screenHeight = 1080;
+    private static List<Pane> panes = new ArrayList<Pane>();
 
     public static void main(String[] args) {
         launch(args);
@@ -17,9 +23,18 @@ public class Main extends Application {
     @Override
     public void start(Stage primaryStage) throws Exception {
 
-        Pane root = new Pane();
+        Pane root = FXMLLoader.load(getClass().getResource("configPane.fxml"));
+        //add background image using css
+        root.setStyle("-fx-background-image: url('" + Main.class.getResource("techMenu.png").toExternalForm() + "');" +
+                " \n-fx-background-position: center center; \n-fx-background-repeat: stretch;");
 
         Scene scene = new Scene(root, screenWidth, screenHeight);
+
+        Button startButton = (Button) scene.lookup("#buttonStart");
+        startButton.setStyle("-fx-background-image: url('" + Main.class.getResource("startButton.png").toExternalForm() + "');" +
+                " \n-fx-background-position: center center; \n-fx-background-repeat: stretch; \n-fx-background-size: stretch;\n" +
+                "\n-fx-border-width: 0;\n -fx-background-radius: 0;\n-fx-background-color: transparent;");
+        startButton.setText(""); //Image already has the text on it so remove it
 
         primaryStage.setScene(scene);
         primaryStage.setTitle("Our Game");
