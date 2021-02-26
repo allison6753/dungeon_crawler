030d81c6b7c7880446a3a75e6952f4e2babfddc8
mayzhao2001 <mzhao306@gatech.edu>
2021-02-26 01:21:31 -0500

    Update gameScreen1.java

--- src/main/gameScreen1.java
+++ src/main/gameScreen1.java
@@ -1,4 +1,28 @@
 package main;
+import javafx.scene.Scene;
+import javafx.scene.layout.Pane;
+import javafx.fxml.FXMLLoader;
+import java.io.IOException;
 
-public class gameScreen1 {
+public class gameScreen1  {
+    private Scene scene;
+    private Pane root;
+
+    public gameScreen1(int width, int height) {
+        try {
+            root = FXMLLoader.load(gameScreen1.class.getResource("../resources/InitialGameScreen.fxml"));
+        } catch (IOException except) {
+            //the fxml loader can't find the file
+        }
+
+        scene = new Scene(root, width, height);
+    }
+
+    public gameScreen1() {}
+
+    public Scene getScene() {
+        return this.scene;
+    }
+
 }
+
