52d9a5fd2b1ce5f6677d9383788d6526c4ad4045
alee455 <alee455@gatech.edu>
2021-04-11 13:12:20 -0400

    add weapon class

---
+++ src/main/Weapon.java
@@ -0,0 +1,22 @@
+package main;
+
+public class Weapon {
+    private int damage;
+    private int cost;
+
+
+
+    public void setDamage(int damage) {
+        this.damage = damage;
+    }
+    public int getDamage() {
+        return damage;
+    }
+
+    public void setCost(int cost) {
+        this.cost = cost;
+    }
+    public int getCost() {
+        return cost;
+    }
+}
